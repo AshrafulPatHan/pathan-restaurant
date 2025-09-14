9172af119b748ee0378a6e6a2995dfbb07b506ef
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-14 22:13:49 +0600

    the pagee is conect on the vite routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import "./index.css"
 import "./page/ui/Ui.css"
+import Error404 from "./page/error/404";
+import Ui from "./page/ui/Ui";
+import BlogPage from "./page/blog/BlogPage";
+import CartPage from "./page/cart/CartPage";
+import ContactPage from "./page/contacts/ContactPage";
+import GalleryPage from "./page/gallery/Gallery";
+import MenuPage from "./page/menu/MenuPage";
+import Reservation from "./page/Reservation/Reservation";
+
 
 const root = document.getElementById("root") as HTMLElement;
 
@@ -10,6 +19,14 @@ ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/ui" element={<Ui />} />
+      <Route path="/blog" element={<BlogPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/gallery" element={<GalleryPage />} />
+      <Route path="/menu" element={<MenuPage />} />
+      <Route path="/reservation" element={<Reservation />} />
+      <Route path="*" element={<Error404 />} />
     </Routes>
   </BrowserRouter>,
 );
