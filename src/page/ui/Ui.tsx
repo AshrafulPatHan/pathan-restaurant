5cda06819beb19a1c9ad53fd8d744973967216ca
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-15 22:29:14 +0600

    add the utilitie class

--- src/page/ui/Ui.tsx
+++ src/page/ui/Ui.tsx
@@ -2,9 +2,18 @@
 
 const Ui = () => {
     return (
-        <div>
+        <div className="">
+            <h2>Color</h2>
+            <div className="flex items-center border border-white/10">
+                <div className="bg-[var(--black)] w-28 h-28  "></div>
+                <div className="bg-[var(--pc)]  w-28 h-28  "></div>
+                <div className="bg-[var(--pc2)] w-28 h-28 "></div>
+                <div className="bg-[var(--pc3)] w-28 h-28 "></div>
+                <div className="bg-[var(--sc)]  w-28 h-28 "></div>
+            </div>
+
+            <h2>Heading</h2>
             <div>
-                <h2>Heading</h2>
                 <div></div>
             </div>
         </div>
