dbf71638b73f955c6ab5ee7c6880dd7d8391d8a1
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-20 21:44:10 +0600

    update the user

--- src/components/home/AboutUs.tsx
+++ src/components/home/AboutUs.tsx
@@ -1,40 +1,68 @@
-import redytabil from "../../assets/image/redytabil.jpg"
-import picake from "../../assets/image/piecake.jpg"
-import itemsmenu from "../../assets/image/menuitems.jpg"
-
+import redytabil from "../../assets/image/redytabil.jpg";
+import picake from "../../assets/image/piecake.jpg";
+import itemsmenu from "../../assets/image/menuitems.jpg";
 
 const AboutUs = () => {
     return (
-        <div className="bg-white flex-center pt-primary">
-            <div className="bg-[#F6F6F6] w-[95%] flex-center ">
-                <div className="w-[80%] bg-white p-6 ">
-                    <div>
-                        <p>Enjoy your time in our Place</p>
-                        <h2>About Our Restaurant</h2>
-                        <hr />
-                        <div>
-                            Qed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam aperiam. Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.Ut enim ad minima Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.
+        <section className="bg-white pt-low">
+            <div className="max-w-[1170px] mx-auto bg-[#F6F6F6] p-4 rounded-2xl ">
+                <div className=" bg-white m-6 p-4 rounded-2xl">
+                    <div className="flex flex-col lg:flex-row items-center lg:items-start gap-10 p-4">
+                        {/* Text Section */}
+                        <div className="lg:w-1/2 space-y-6">
+                            <p className="text-c-sec italic font-semibold">
+                                Enjoy your time in our Place
+                            </p>
+                            <h2 className="text-3xl lg:text-4xl font-bold">
+                                About Our Restaurant
+                            </h2>
+                            <hr className="border-t-2 border-dotted border-gray-400 my-4" />
+                            <p className="text-gray-600 leading-relaxed">
+                                Qed ut perspiciatis unde omnis iste natus error sit voluptatem
+                                accusantium doloremque laudantium totam aperiam. Eaque ipsa quae
+                                ab illo inventore veritatis et quasi architecto beatae vitae dicta
+                                sunt. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+                                corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
+                                consequatur.
+                            </p>
+                            <p className="text-gray-600 leading-relaxed">
+                                Fames massa tortor sit nisl sit. Duis nulla tempus quisque et diam
+                                condimentum nisl. Rhoncus quisque elementum nulla lorem at turpis
+                                vitae quisque. Vulputate duis vel et odio hendrerit magna. Nec
+                                lacus dui egestas sit. Vulputate tincidunt viverra viverra etiam
+                                porta facilisis.
+                            </p>
+                            <button className="btn-sec">Read more About Us</button>
+                        </div>
 
-                            Fames massa tortor sit nisl sit. Duis nulla tempus quisque et diam condimentum nisl. Rhoncus quisque elementum nulla lorem at turpis vitae quisque. Vulputate duis vel et odio hendrerit magna. Nec lacus dui egestas sit. Vulputate tincidunt viverra viverra etiam porta facilisis. Fames massa tortor sit nisl sit. Duis nulla tempus quisque et diam condimentum nisl. Rhoncus quisque elementum nulla lorem at turpis vitae quisque. Vulputate duis vel et odio hendrerit magna.
+                        {/* Image Section */}
+                        <div className="relative lg:w-1/2 flex justify-center items-center">
+                            {/* Background dotted box */}
+                            <div className="absolute w-[380px] h-[380px] rounded-2xl border border-dotted border-[#bda9a9e1] -top-6 -left-6"></div>
+
+                            {/* Main Image */}
+                            <img
+                                src={redytabil}
+                                alt="our tables"
+                                className="w-[500px] h-[350px] rounded-xl object-cover relative z-10 shadow-md"
+                            />
+
+                            {/* Small Overlay Images */}
+                            <img
+                                src={picake}
+                                alt="cake"
+                                className="absolute bottom-[-40px] left-[-40px] w-[200px] h-[130px] rounded-xl object-cover shadow-lg z-20"
+                            />
+                            <img
+                                src={itemsmenu}
+                                alt="menu items"
+                                className="absolute top-[-40px] right-[-40px] w-[200px] h-[130px] rounded-xl object-cover shadow-lg z-20"
+                            />
                         </div>
-                        <div className="btn-sec">Read more About Us </div>
-                    </div>
-                    <div>
-                        <div>
-                            <img src={redytabil} alt="" />
-                        </div>
-                        <div>
-                            <img src={picake} alt="" />
-                        </div>
-                        <div>
-                            <img src={itemsmenu} alt="" />
-                        </div>
-                        <div></div>
                     </div>
                 </div>
             </div>
-
-        </div>
+        </section>
     );
 };
 
--- src/components/home/PopularBlog.tsx
+++ src/components/home/PopularBlog.tsx
@@ -3,7 +3,30 @@
 const PopularBlog = () => {
     return (
         <div>
-            
+            <div>
+                <div>
+                    <div>
+                        <img src="" alt="photo" />
+                    </div>
+                    <div>
+                        <p>66</p>
+                        <p>
+                            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi accusantium."
+                        </p>
+                        <p>Kevin Kowalsky - Restaurant's cheaf</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h2>Our Last News</h2>
+                        <p>Upcoming Events & News</p>
+                        <div></div>
+                        <div>
+                            <button className="btn-primary">Read more blog</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
