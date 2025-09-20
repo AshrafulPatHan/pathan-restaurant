460ceca8a5c4ff070b7e3dc10e52cf5e43955c86
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-20 18:28:50 +0600

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.css'
+import AboutUs from './components/home/AboutUs'
+import Clients from './components/home/Clients'
+import Hero from './components/home/Hero'
+import Menu_offers from './components/home/Menu_offers'
+import PopularBlog from './components/home/PopularBlog'
+import Service from './components/home/Service'
+import Footer from './components/navigation/Footer'
+import Navbar from './components/navigation/Navbar'
 
 function App() {
 
   return (
     <div>
-      Hello world
+      <Navbar/>
+      <Hero/>
+      <AboutUs/>
+      <PopularBlog/>
+      <Service/>
+      <Menu_offers/>
+      <Clients/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/home/Menu_offers.tsx
@@ -0,0 +1,11 @@
+
+
+const Menu_offers = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Menu_offers;
