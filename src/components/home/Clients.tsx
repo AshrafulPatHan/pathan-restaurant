489e50aad39b69a2709498bdc03be03b0cda224e
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-09-29 20:00:26 +0600

    add the popular menu

--- src/components/home/Clients.tsx
+++ src/components/home/Clients.tsx
@@ -3,7 +3,31 @@
 const Clients = () => {
     return (
         <div>
-            
+            <p>Enjoy your time in our Restaurant with pleasure.</p>
+            <h2>Testimonials & Clients</h2>
+            <div>
+                <div className="relative">
+                    <div>
+                        <img src="https://i.ibb.co.com/GvWGMT4T/Screenshot-20250712-211018.png" alt="" />
+                    </div>
+                    <h4>clinte</h4>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatum nostrum, tempore tempora consequuntur quo debitis maxime dolores ut sint.
+                    </p>
+                    <p></p>
+                    <div>
+                        <p></p>
+                        <div>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                        </div>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/home/Menu_offers.tsx
+++ src/components/home/Menu_offers.tsx
@@ -1,11 +1,58 @@
-
+import "./home.css";
 
 const Menu_offers = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="offers-images relative flex items-center justify-center pb-h bg-black/60">
+      <div className="max-w-[1500px] w-full flex text-white pt-low px-6">
+        {/* Left Side */}
+        <div className="w-[35%] pr-12">
+          <h2 className="text-sm text-c-sec font-bold italic mb-2">
+            Special menu offers.
+          </h2>
+          <p className="text-4xl font-bold leading-snug mb-4">
+            Enjoy Restaurants <br /> Specialties
+          </p>
+          <p className="text-sm text-gray-300 mb-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in
+            pulvinar neque. Nulla finibus lobortis pulvinar.
+          </p>
+          <button className="px-6 py-3 bg-yellow-600 hover:bg-yellow-700 text-white font-semibold rounded">
+            View Full Menu
+          </button>
         </div>
-    );
+
+        {/* Divider */}
+        <div className="w-[1px] bg-white/30 mx-8"></div>
+
+        {/* Right Side */}
+        <div className="flex flex-col gap-6 w-[55%]">
+          {[
+            { no: "01.", name: "Soft shell crab", price: "$29" },
+            { no: "02.", name: "Victoria's Filet Mignon", price: "$49" },
+            { no: "03.", name: "Salmon Riverland", price: "$12" },
+            { no: "04.", name: "Prime Cuts of Beef", price: "$107" },
+            { no: "05.", name: "Miso chicken", price: "$46" },
+          ].map((item, index) => (
+            <div key={index} className="flex gap-6 items-start">
+              <p className="text-c-sec font-semibold">{item.no}</p>
+              <div className="flex-1">
+                <div className="flex items-center">
+                  <p className="font-semibold text-lg whitespace-nowrap">
+                    {item.name}
+                  </p>
+                  <div className="flex-1 border-b border-dotted border-white/40 mx-4"></div>
+                  <p className="text-c-sec font-semibold">{item.price}</p>
+                </div>
+                <p className="text-gray-400 text-sm mt-1">
+                  Granny help you treat yourself with a different meal everyday
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Menu_offers;
